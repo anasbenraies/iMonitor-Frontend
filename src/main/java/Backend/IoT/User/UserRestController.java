5971227c219c012742ff1f4a2d32784853db8ca4
anas ben raies <a.benraies19038@pi.tn>
2024-06-28 18:05:52 +0100

    n

--- src/main/java/Backend/IoT/User/UserRestController.java
+++ src/main/java/Backend/IoT/User/UserRestController.java
@@ -4,10 +4,7 @@ package Backend.IoT.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +23,7 @@ public class UserRestController {
 
 
 
-    @GetMapping(path = "/user")
+    @PostMapping(path = "/user")
     public ResponseEntity<User> getUser(@RequestBody User u){
         try {
             //this is the user taht we are looking for
