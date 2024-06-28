b79422710bfbaf92dc427888ac65a10dea014c2e
anas ben raies <a.benraies19038@pi.tn>
2024-06-28 17:47:08 +0100

    just added user

---
+++ src/main/java/Backend/IoT/User/User.java
@@ -0,0 +1,43 @@
+package Backend.IoT.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+
+    @Column(name = "username",nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email ;
+    private String password ;
+    private LocalDate dob;
+
+
+}
